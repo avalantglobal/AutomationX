9077440361d50a9a450fec0101d07d84c52c8195
tanakit <tum_tanakid@hotmail.com>
2025-04-17 06:27:26 +0000

    feat(analytics): create 2 APIs, /analytics/workflow-performance and /analytics/overview

--- packages/server/api/src/app/analytics/analytics-module.ts
+++ packages/server/api/src/app/analytics/analytics-module.ts
@@ -1,6 +1,6 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync, FastifyInstance } from 'fastify'
 import { analyticsController } from './analytics-controller'
 
-export const analyticsModule: FastifyPluginAsync = async (app) => {
+export const analyticsModule: FastifyPluginAsync = async (app: FastifyInstance) => {
     await app.register(analyticsController, { prefix: '/v1/analytics' })
 }

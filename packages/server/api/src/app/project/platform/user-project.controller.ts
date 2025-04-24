eef357048a1ef14672de12304fd29175d6a1d232
Rupal <rupalbarman@gmail.com>
2025-04-24 13:31:48 +0700

    feat: add support for platform and user projects on backend

---
+++ packages/server/api/src/app/project/platform/user-project.controller.ts
@@ -0,0 +1,84 @@
+/*
+This is a custom implementation to support platform projects with access control and other features.
+The logic has been isolated to this file to avoid potential conflicts with the open-source modules from upstream
+*/
+
+import { assertNotNullOrUndefined, ListProjectRequestForUserQueryParams, PrincipalType, Project, ProjectsWithPlatform, SeekPage } from '@activepieces/shared'
+import { FastifyPluginAsyncTypebox, Type } from '@fastify/type-provider-typebox'
+import { StatusCodes } from 'http-status-codes'
+import { platformService } from '../../platform/platform.service'
+import { platformUtils } from '../../platform/platform.utils'
+import { userService } from '../../user/user-service'
+import { platformProjectService } from './platform-project.service'
+
+export const userPlatformProjectController: FastifyPluginAsyncTypebox = async (fastify) => {
+    // Overrides the same endpoint handler in the open source counter-part
+    fastify.get('/:id', async (request) => {
+        return platformProjectService(request.log).getWithPlanAndUsageOrThrow(request.principal.projectId)
+    })
+
+    // Overrides the same endpoint handler in the open source counter-part
+    fastify.get('/', ListProjectRequestForUser, async (request) => {
+        return platformProjectService(request.log).getAllForPlatform({
+            platformId: request.principal.platform.id,
+            userId: request.principal.id,
+            cursorRequest: request.query.cursor ?? null,
+            displayName: request.query.displayName,
+            limit: request.query.limit ?? 10,
+        })
+    })
+
+    fastify.get('/platforms', ListProjectsForPlatforms, async (request) => {
+        const loggedInUser = await userService.getOneOrFail({ id: request.principal.id })
+        const platforms = await getPlatformsForUser(loggedInUser.identityId, request.principal.platform.id)
+        const projectsWithPlatform = await Promise.all(platforms.map(async (platform) => {
+            const platformUser = await userService.getOneByIdentityAndPlatform({ identityId: loggedInUser.identityId, platformId: platform.id })
+            assertNotNullOrUndefined(platformUser, `Platform user not found for platform ${platform.id}`)
+            const projects = await platformProjectService(request.log).getAllForPlatform({
+                platformId: platform.id,
+                userId: platformUser.id,
+                cursorRequest: null,
+                displayName: undefined,
+                limit: 1000,
+            }).then((projects) => projects.data)
+            return {
+                platformName: platform.name,
+                projects,
+            }
+        }))
+        return projectsWithPlatform.flat()
+    })
+}
+
+async function getPlatformsForUser(identityId: string, platformId: string) {
+    const platform = await platformService.getOneOrThrow(platformId)
+    if (platformUtils.isEnterpriseCustomerOnCloud(platform)) {
+        return [platform]
+    }
+    const platforms = await platformService.listPlatformsForIdentityWithAtleastProject({ identityId })
+    return platforms.filter((platform) => !platformUtils.isEnterpriseCustomerOnCloud(platform))
+}
+
+const ListProjectRequestForUser = {
+    config: {
+        allowedPrincipals: [PrincipalType.USER],
+    },
+    schema: {
+        response: {
+            [StatusCodes.OK]: SeekPage(Project),
+        },
+        querystring: ListProjectRequestForUserQueryParams,
+    },
+}
+
+const ListProjectsForPlatforms = {
+    config: {
+        allowedPrincipals: [PrincipalType.USER],
+    },
+    schema: {
+        response: {
+            [StatusCodes.OK]: Type.Array(ProjectsWithPlatform),
+        },
+    },
+}
+

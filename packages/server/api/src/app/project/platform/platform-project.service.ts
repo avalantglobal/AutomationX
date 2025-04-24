60d757c306af67d7651549c93a1a0e3b15e31903
Rupal <rupalbarman@gmail.com>
2025-04-24 15:30:58 +0700

    chore: add the file comment that was missing

--- packages/server/api/src/app/project/platform/platform-project.service.ts
+++ packages/server/api/src/app/project/platform/platform-project.service.ts
@@ -1,3 +1,8 @@
+/*
+This is a custom implementation to support platform projects with access control and other features.
+The logic has been isolated to this file to avoid potential conflicts with the open-source modules from upstream
+*/
+
 import {
     ActivepiecesError, assertNotNullOrUndefined,
     Cursor,

f2f0e47b4853d9bf30e11f8b17f76f20d68aaa74
tanakit <tum_tanakid@hotmail.com>
2025-06-13 16:34:48 +0700

    feat(ms-outlook): add CustomAPI action

--- packages/pieces/community/microsoft-outlook-email/src/index.ts
+++ packages/pieces/community/microsoft-outlook-email/src/index.ts
@@ -1,4 +1,5 @@
-import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createPiece, PieceAuth,OAuth2PropertyValue } from '@activepieces/pieces-framework';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { newEmailTrigger } from './lib/triggers/new-email';
 import { sendEmail } from './lib/actions/send-email';
 import { replyEmail } from './lib/actions/reply-email';
@@ -21,6 +22,14 @@ export const microsoftOutlookEmail = createPiece({
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/microsoft-outlook.png',
   authors: [],
-  actions: [sendEmail, replyEmail],
+  actions: [sendEmail, replyEmail,
+    createCustomApiCallAction({
+      baseUrl: () => 'https://graph.microsoft.com/v1.0/',
+      auth: outlookEmailAuth,
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
+      }),
+    })
+  ],
   triggers: [newEmailTrigger],
 });
